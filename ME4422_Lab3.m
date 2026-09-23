a0aa9eb77face988a071c335fd01fdc1d3d7c77c
farahmichael05 <farahmichael05@gmail.com>
2026-09-23 11:23:30 -0400

    Update ME4422_Lab3.m

--- ME4422_Lab3.m
+++ ME4422_Lab3.m
@@ -38,11 +38,12 @@ J_leverarm = 1;
 J_1 = 1;
 
 %Dampers
-D_p1 = 1; %Friction with Pin 1
-D_p2 = 1; %Friction with Pin 2
-D_3 = 1;  %Friction with rack and ground
-D_4 = 1;  %Friction with pinion and display
-D_B = 1;  %Platform, M_E friction?
+D_P = 1; %Friction with Pin P
+D_R = 1;  %Friction with rack and ground
+D_pin = 1;  %Friction with pinion 
+D_D = 1;  %friction at pivot D
+D_A = 1;  %friction at pivot A
+D_B = 1;  %Platform
 
 %Spring Stiffness
 
@@ -62,7 +63,6 @@ M_eq2 = J_pinion + (R^2)*M_eq1; %Lump to Pinion (Translation --> Rotation)
 M_eq = (1/R^2)*(M_eq2) + (1/3)*(Mk_4); %Lump to k4 (Rotation --> Translation)
 
 % Moment of Inertia Equivalence -- Pinion's location (Torsional)
-%Comment consistency will be fixed later
 
 J_eq1 = J_1 + M_E*L1^2; % Lump bar E to pin between bar E and bar F
 J_eq2 = J_2 + M_F*L4^2; % Lump bar F to ground
@@ -91,13 +91,16 @@ k_eq9T = k_eq8R/(L7^2) + k_4; %move to S and add k_4
 k_eq = (R^2)*k_eq9T; %move to pinion
 
 %Damper Equivalence -- Pinion's Location (Torsional)
-%This is most likely wrong, I rushed it, can fix tomorrow morning
 
-D_eq1R = D_B*L6^2 + D_p1; %Lump Platform damper to P, Translation to Rotation
-D_eq2R = D_eq1R + D_p2; %add damper at S 
-D_eq3T = (D_eq2R)* (1/L8^2); %to rack
+D_eq1 = D_D/(L3^2); %Lump D to C
+D_eq2 = D_eq1 * (L5^2); %Lump C to A
+D_eq3 = (D_A + D_eq2)/(L2^2); %Add A and move to B
+D_eq4 = 2*D_eq3; %Both Sides
+D_eq5 = D_eq4 + D_B;%Add platform damper
+D_eq6 = D_eq5*(L6^2) + D_P; %Lump B to P
+D_eq7 = D_eq6/(L7^2) + D_R; %Lump P to S and add Rack
 
-D_eq = (D_eq3T)*(R^2) + D_4; %To pinion, Translation --> Rotation
+D_eq = D_eq7*(R^2) + D_pin; %Lump to Pinion
 
 %% Model Solver and Plotting
 
